ebdced7b1c09d952cd6476c26a828889c639d530
frankNowinski <nowinski.frank@gmail.com>
2015-09-30 17:15:29 -0400

    Update roman_numeral.rb

--- roman_numeral.rb
+++ roman_numeral.rb
@@ -1,33 +1,34 @@
+#  This is a simple ruby program that converts Roman Numerals into integers.
+
 def valid(array)
-	roman_numeral = { }
+  roman_numeral = { }
 
-	roman_numeral['I'] = 1
-	roman_numeral['V'] = 5
-	roman_numeral['X'] = 10
-	roman_numeral['L'] = 50
-	roman_numeral['C'] = 100
-	roman_numeral['D'] = 500
-	roman_numeral['M'] = 1000
+  roman_numeral['I'] = 1
+  roman_numeral['V'] = 5
+  roman_numeral['X'] = 10
+  roman_numeral['L'] = 50
+  roman_numeral['C'] = 100
+  roman_numeral['D'] = 500
+  roman_numeral['M'] = 1000
 
-	sum = 0
-	count = 0
-	len = array.length
+  sum = 0
+  count = 0
+  len = array.length
 
-	array.each { |input|
+  array.each { |input|
+    roman_numeral.each { |key, value|
+      if input.chr == key 
+        sum += value
+	count += 1
+      end
+    }
+  }
 
-		roman_numeral.each { |key, value|
-			if input.chr == key 
-				sum += value
-				count += 1
-			end
-		}
-	}
-
-	if count != len
-		puts"Invalid Roman Numeral"
-		exit
-	end 
-	sum	
+  if count != len
+    puts"Invalid Roman Numeral"
+    exit
+  end 
+  sum	
 end
 
 puts "Please enter a Roman Numeral: "
@@ -38,8 +39,8 @@ len = numeral.length
 array = []
 
 while count < len
-	array.push(numeral[count])
-	count += 1
+  array.push(numeral[count])
+  count += 1
 end
 
 puts "Roman Numeral #{numeral} converted into an integer is #{valid(array)}"
